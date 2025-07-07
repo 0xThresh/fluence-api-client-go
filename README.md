a64e8eb3620c7d3aebb3a9e78e3991ac08fa0d99
0xThresh.eth <0xthresh@protonmail.com>
2025-07-07 12:09:05 -0600

    feat: update Fluence Go client to use latest API spec (v0.8.64)

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # Fluence Golang API Client
 
-A Go client package that provides connections to the [Fluence API](https://api.fluence.dev/docs).
+A Go client package that provides connections to the [Fluence API](https://api.fluence.dev/docs) (v0.8.64).
 
 Using this module, the Fluence provider establishes a new client and sends HTTP(s) requests to the Fluence API to perform CRUD operations. It also handles data mapping from user's inputs to `models.go`. The Fluence URL defaults to `https://api.fluence.dev/`, and is set in the `client.go` file.
+
+## Features
+
+This client supports all Fluence API endpoints including:
+
+- **SSH Key Management**: List, create, and remove SSH keys
+- **Virtual Machine Management**: Create, list, update, remove, and get status of VMs
+- **Marketplace Operations**: Browse available configurations, countries, hardware, and offers
+- **Resource Management**: Estimate deposits and get default OS images
 
 ## Usage
 Using the client requires a valid Fluence API key. Keys can be created in the [API keys](https://console.fluence.network/settings/api-keys) page in the Fluence console.
@@ -10,4 +19,4 @@ Using the client requires a valid Fluence API key. Keys can be created in the [A
 Once you have an API key, set the `FLUENCE_API_KEY` environment variable:
 `export FLUENCE_API_KEY=<your key>`
 
-Finally, you can run `go run test/test.go` to validate your authentication, which should return any SSH keys you've uploaded, and any running VMs in your account. 
+Finally, you can run `go run test/test.go` to validate your authentication, which should return any SSH keys you've uploaded, running VMs, available configurations, and other marketplace data. 
--- test/test.go
+++ test/test.go
@@ -1,38 +1,71 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 
-    fluenceapi "github.com/0xthresh/fluence-api-client-go"
+	fluenceapi "github.com/0xthresh/fluence-api-client-go"
 )
 
 func main() {
-    apiKey := os.Getenv("FLUENCE_API_KEY")
-    if apiKey == "" {
-        fmt.Println("Please set the FLUENCE_API_KEY environment variable.")
-        return
-    }
+	apiKey := os.Getenv("FLUENCE_API_KEY")
+	if apiKey == "" {
+		fmt.Println("Please set the FLUENCE_API_KEY environment variable.")
+		return
+	}
 
-    client, err := fluenceapi.NewClient(nil, &apiKey)
-    if err != nil {
-        fmt.Printf("Error creating client: %v\n", err)
-        return
-    }
+	client, err := fluenceapi.NewClient(nil, &apiKey)
+	if err != nil {
+		fmt.Printf("Error creating client: %v\n", err)
+		return
+	}
 
-    // List SSH keys
-    sshKeys, err := client.ListSshKeys()
-    if err != nil {
-        fmt.Printf("Error listing SSH keys: %v\n", err)
-    } else {
-        fmt.Printf("SSH Keys: %+v\n", sshKeys)
-    }
+	// List SSH keys
+	sshKeys, err := client.ListSshKeys()
+	if err != nil {
+		fmt.Printf("Error listing SSH keys: %v\n", err)
+	} else {
+		fmt.Printf("SSH Keys: %+v\n", sshKeys)
+	}
 
-    // List running VMs
-    vms, err := client.ListVmsV3()
-    if err != nil {
-        fmt.Printf("Error listing VMs: %v\n", err)
-    } else {
-        fmt.Printf("Running VMs: %+v\n", vms)
-    }
+	// List running VMs
+	vms, err := client.ListVmsV3()
+	if err != nil {
+		fmt.Printf("Error listing VMs: %v\n", err)
+	} else {
+		fmt.Printf("Running VMs: %+v\n", vms)
+	}
+
+	// Test new endpoints
+	// Get default OS images
+	images, err := client.GetDefaultImages()
+	if err != nil {
+		fmt.Printf("Error getting default images: %v\n", err)
+	} else {
+		fmt.Printf("Default Images: %+v\n", images)
+	}
+
+	// Get basic configurations
+	configs, err := client.GetBasicConfigurations()
+	if err != nil {
+		fmt.Printf("Error getting basic configurations: %v\n", err)
+	} else {
+		fmt.Printf("Basic Configurations: %+v\n", configs)
+	}
+
+	// Get available countries
+	countries, err := client.GetAvailableCountries()
+	if err != nil {
+		fmt.Printf("Error getting available countries: %v\n", err)
+	} else {
+		fmt.Printf("Available Countries: %+v\n", countries)
+	}
+
+	// Get available hardware
+	hardware, err := client.GetAvailableHardware()
+	if err != nil {
+		fmt.Printf("Error getting available hardware: %v\n", err)
+	} else {
+		fmt.Printf("Available Hardware: %+v\n", hardware)
+	}
 }
